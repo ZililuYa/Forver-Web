e4823fa488ed07cf320152b2ad652b0baadb2f3b
nesfe <1029131145@qq.com>
2017-09-20 17:37:49 +0800

    sc

---
+++ views/src/App.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import VueHighlight from 'vue-highlight.js'
+import F from 'forver'
+import 'forver/dist/css/forver.min.css'
+import 'highlight.js/styles/monokai-sublime.css'
+
+Vue.config.productionTip = false
+Vue.use(VueHighlight)
+Vue.use(F)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: {App}
+})
